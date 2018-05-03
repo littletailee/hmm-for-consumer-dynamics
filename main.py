fix bug for softmax

main.py
@@ -104,12 +104,14 @@ def hmm(observed, x, a, z, n_batch, n_steps, n_particles):
 
             logits = tf.reduce_sum(a[tf.newaxis, :, i, tf.newaxis, tf.newaxis, :] *
                                    rho[:, tf.newaxis, :, tf.newaxis, :], axis=4) + mu
-            Q = tf.stack([
-                tf.sigmoid(logits[:, :, :, 0]),
-                tf.sigmoid(logits[:, :, :, 1]) -
-                tf.sigmoid(logits[:, :, :, 0]),
-                1 - tf.sigmoid(logits[:, :, :, 1])
-            ], axis=3)
+            Q = tf.nn.softmax(
+                tf.concat([logits, tf.zeros_like(logits[:, :, :, 0, tf.newaxis])], axis=3), axis=3)
+            # Q=tf.stack([
+            #     tf.sigmoid(logits[:, :, :, 0]),
+            #     tf.sigmoid(logits[:, :, :, 1]) -
+            #     tf.sigmoid(logits[:, :, :, 0]),
+            #     1 - tf.sigmoid(logits[:, :, :, 1])
+            # ], axis = 3)
             # Q = tf.nn.softmax(logits, axis=3)
             prob = tf.reduce_sum(
                 tf.multiply(Q, prob[:, :, :, tf.newaxis]),
@@ -143,7 +145,7 @@ if __name__ == '__main__':
     n_chains = 10
     n_burnin = 10000
     n_sample = 1000
-    n_leapfrogs = 10
+    n_leapfrogs = 5
 
     x = tf.placeholder(tf.float32, shape=[n_batch, T, X_DIM])
     a = tf.placeholder(tf.float32, shape=[n_batch, T, A_DIM])
@@ -184,16 +186,6 @@ if __name__ == '__main__':
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-
-    # lr_value = 1e-2
-    # for i in range(1000):
-    #     if i > 100:
-    #         lr_value = 1e-3
-    #     if i > 500:
-    #         lr_value = 1e-4
-    #     _, loss = sess.run([train_op, loss_op], feed_dict={
-    #                        lr: lr_value, y: y_it, x: x_it, a: a_it, z: z_it})
-    #     print('Iter {}: Loss = {}'.format(i, loss))
 
     print('Burning...')
     for i in range(n_burnin):
@@ -257,8 +249,8 @@ if __name__ == '__main__':
     print('beta0 mean = {}'.format(np.mean(beta0, axis=1)))
     print('beta0 stdev = {}'.format(np.std(beta0, axis=1)))
 
-    # print('delta mean = {}'.format(np.mean(delta, axis=0)))
-    # print('delta stdev = {}'.format(np.std(delta, axis=0)))
+    print('delta mean = {}'.format(np.mean(delta, axis=1)))
+    print('delta stdev = {}'.format(np.std(delta, axis=1)))
 
     print('rho mean = {}'.format(np.mean(rho, axis=1)))
     print('rho stdev = {}'.format(np.std(rho, axis=1)))

q.py
@@ -7,6 +7,51 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def softmax(X, theta=1.0, axis=None):
+    """
+    Compute the softmax of each element along an axis of X.
+
+    Parameters
+    ----------
+    X: ND-Array. Probably should be floats. 
+    theta (optional): float parameter, used as a multiplier
+        prior to exponentiation. Default = 1.0
+    axis (optional): axis to compute values along. Default is the 
+        first non-singleton axis.
+
+    Returns an array the same size as X. The result will sum to 1
+    along the specified axis.
+    """
+
+    # make X at least 2d
+    y = np.atleast_2d(X)
+
+    # find axis
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+
+    # multiply y against the theta parameter,
+    y = y * float(theta)
+
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis=axis), axis)
+
+    # exponentiate y
+    y = np.exp(y)
+
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis=axis), axis)
+
+    # finally: divide elementwise
+    p = y / ax_sum
+
+    # flatten if X was 1D
+    if len(X.shape) == 1:
+        p = p.flatten()
+
+    return p
+
+
 def main():
     a_it, x_it, y_it, z_it = read_data()
     a_it = a_it[:, :, [0, 1]]
@@ -15,7 +60,7 @@ def main():
     T = 23
     n_batch = a_it.shape[0]
 
-    data = np.load('param-10000-5.npz')
+    data = np.load('param1-10000-2.npz')
     delta = data['delta']
     rho = data['rho']
 
@@ -31,12 +76,16 @@ def main():
     for i in range(T):
         logits = np.sum(a_it[:, i, np.newaxis, np.newaxis, :] *
                         rho[np.newaxis, :, np.newaxis, :], axis=3) + mu
-        q = np.stack([
-            sigmoid(logits[:, :, 0]),
-            sigmoid(logits[:, :, 1]) -
-            sigmoid(logits[:, :, 0]),
-            1 - sigmoid(logits[:, :, 1])
-        ], axis=2).mean(axis=0)
+        q = softmax(
+            np.concatenate([logits, np.zeros_like(
+                logits[:, :, 0, np.newaxis])], axis=2),
+            axis=2).mean(axis=0)
+        # q = np.stack([
+        #     sigmoid(logits[:, :, 0]),
+        #     sigmoid(logits[:, :, 1]) -
+        #     sigmoid(logits[:, :, 0]),
+        #     1 - sigmoid(logits[:, :, 1])
+        # ], axis=2).mean(axis=0)
         qs.append(q)
 
     q_mean = np.mean(qs, axis=0)